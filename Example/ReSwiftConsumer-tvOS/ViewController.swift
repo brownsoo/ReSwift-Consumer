6cfb163d1e0b38c9cd202fc1d1d28b756d64d391
brownsoo han <hansune@me.com>
2018-01-02 15:43:21 +0900

    Add target tvOS

---
+++ Example/ReSwiftConsumer-tvOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwiftConsumer-tvOS
+//
+//  Created by hyonsoo han on 2018. 1. 2..
+//  Copyright © 2018년 HansooLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
