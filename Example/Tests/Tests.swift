6c908928d9fb47efe4a4437382e748add970de66
hyonsoo han <hansune@me.com>
2018-03-29 18:18:45 +0900

    update

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,51 +1,19 @@
 // https://github.com/Quick/Quick
 
-import Quick
-import Nimble
-import ReSwiftConsumer
+import XCTest
+@testable import ReSwiftConsumer
 
-class TableOfContentsSpec: QuickSpec {
-    override func spec() {
-        
-//        describe("these will fail") {
-//
-//            it("can do maths") {
-//                expect(1) == 2
-//            }
-//
-//            it("can read") {
-//                expect("number") == "string"
-//            }
-//
-//            it("will eventually fail") {
-//                expect("time").toEventually( equal("done") )
-//            }
-//
-//            context("these will pass") {
-//
-//                it("can do maths") {
-//                    expect(23) == 23
-//                }
-//
-//                it("can read") {
-//                    expect("🐮") == "🐮"
-//                }
-//
-//                it("will eventually pass") {
-//                    var time = "passing"
-//
-//                    DispatchQueue.main.async {
-//                        time = "done"
-//                    }
-//
-//                    waitUntil { done in
-//                        Thread.sleep(forTimeInterval: 0.5)
-//                        expect(time) == "done"
-//
-//                        done()
-//                    }
-//                }
-//            }
-//        }
+class Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testSample() {
+        XCTAssertTrue("test".lengthOfBytes(using: String.Encoding.utf8) == 4)
     }
 }
