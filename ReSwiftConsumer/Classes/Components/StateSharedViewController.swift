d0cb09750f84612a5195462a4f8cd58753826462
brownsoo han <hansune@me.com>
2019-02-01 16:09:10 +0900

    changed deprecated ver

--- ReSwiftConsumer/Classes/Components/StateSharedViewController.swift
+++ ReSwiftConsumer/Classes/Components/StateSharedViewController.swift
@@ -16,12 +16,12 @@ open class StateSharedViewController<State>: UIViewController, StateShared
     
     private(set) open var sharedStore: Store<State>?
 
-    @available(*, deprecated, message: "Make and use own pageConsumer instead for this in view controller. This will be removed at version 0.5")
+    @available(*, deprecated, message: "Make and use own pageConsumer instead for this in view controller. This will be removed at version 0.6")
     private(set) open var sharedConsumer: StateConsumer<State>?
 
     public let pageConsumer = StateConsumer<State>()
 
-    @available(*, deprecated, obsoleted: 0.5, message: "This is useless because the sharedConsumer is deprecated.")
+    @available(*, deprecated, obsoleted: 0.6, message: "This is useless because the sharedConsumer is deprecated.")
     public lazy var consumerBag: ConsumerBag<State>? = { [weak sharedConsumer] in
         guard let sharedConsumer = sharedConsumer else {
             return nil
