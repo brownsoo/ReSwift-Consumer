1c33f795313477e801cab99e81691f8aa949d562
brownsoo han <hansune@me.com>
2019-08-28 16:22:26 +0900

    changed Project structure

---
+++ ReSwiftConsumer/ReSwiftConsumer.h
@@ -0,0 +1,19 @@
+//
+//  ReSwiftConsumer.h
+//  ReSwiftConsumer
+//
+//  Created by brownsoo han on 2019/08/28.
+//  Copyright © 2019 brownsoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReSwiftConsumer.
+FOUNDATION_EXPORT double ReSwiftConsumerVersionNumber;
+
+//! Project version string for ReSwiftConsumer.
+FOUNDATION_EXPORT const unsigned char ReSwiftConsumerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReSwiftConsumer/PublicHeader.h>
+
+
